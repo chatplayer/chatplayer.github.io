684ea68a619bd1acd958d67d55be75d40b4c05bc
MD <mattdwnet@gmail.com>
2017-02-23 14:46:43 +0100

    fix app path

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ System.config({
   defaultJSExtensions: true,
   transpiler: false,
   paths: {
-    "*": "/*",
+    "*": "app/*",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
